b7a161e8b12b475fdb054c9a47c6ff33574b2fef
Adrián Font Romero <36204357+AdrianFR99@users.noreply.github.com>
2019-09-19 20:57:06 +0200

    started setting up imgui

--- 3D_Engine/ModuleEngineUI.cpp
+++ 3D_Engine/ModuleEngineUI.cpp
@@ -21,13 +21,53 @@ bool ModuleEngineUI::Init() {
 
 bool ModuleEngineUI::Start() {
 
-	Init_test_UI();
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
 
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL3_Init();
 
 	return true;
 }
 
+update_status  ModuleEngineUI::PreUpdate(float dt) {
 
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+
+	return UPDATE_CONTINUE;
+}
+update_status ModuleEngineUI::Update(float dt) {
+
+
+
+	return UPDATE_CONTINUE;
+}
+update_status  ModuleEngineUI::PostUpdate(float dt) {
+
+	return UPDATE_CONTINUE;
+}
+bool  ModuleEngineUI::CleanUp() {
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+
+	return true;
+	
+}
+
+void  ModuleEngineUI::Draw() const {
+
+	
+
+
+}
 
 void ModuleEngineUI::Init_test_UI() {
 
--- 3D_Engine/ModuleEngineUI.h
+++ 3D_Engine/ModuleEngineUI.h
@@ -13,10 +13,20 @@ public:
 public:
 
 	bool Init();
-
 	bool Start();
+	update_status PreUpdate(float dt);
+	update_status Update(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+
+	void Draw() const;
+
 
 public:
+	
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	void Init_test_UI();
 
