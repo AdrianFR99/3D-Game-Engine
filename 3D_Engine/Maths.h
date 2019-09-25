b31d7dfebd8fbc6ed8acd216fb41ce0594a32726
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2019-09-25 21:05:56 +0200

    Add files needed

---
+++ 3D_Engine/Maths.h
@@ -0,0 +1,10 @@
+#ifndef __MATHS_H__
+#define __MATHS_H_
+
+
+//#include "MathGeoLib/include/MathGeoLibFwd.h"
+#include "MathGeoLib/include/MathGeoLib.h"
+#include "MathGeoLib/include/MathBuildConfig.h"
+
+
+#endif // __MATHS_H__
--- 3D_Engine/ModuleEngineUI.cpp
+++ 3D_Engine/ModuleEngineUI.cpp
@@ -9,6 +9,9 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 
+#include "maths.h"
+
+
 bool show_demo_window = true;
 bool show_another_window = false;
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
