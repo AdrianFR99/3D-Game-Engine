df32948ee7a785cd4793f69f865148d95eca3e84
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2019-10-23 19:09:28 +0200

    set bases of comonet mesh

---
+++ 3D_Engine/ComponentMesh.cpp
@@ -0,0 +1,36 @@
+#include "ComponentMesh.h"
+
+#include "Component.h"
+
+
+ComponentMesh::ComponentMesh(Gameobject * owner, CompType newtype) : Component(newtype, owner)
+{
+	//set name and type
+
+}
+
+ComponentMesh::~ComponentMesh()
+{
+}
+
+void ComponentMesh::Init()
+{
+}
+
+void ComponentMesh::Update()
+{
+
+}
+
+void ComponentMesh::CleanUp()
+{
+	
+}
+
+void ComponentMesh::Enable()
+{
+}
+
+void ComponentMesh::Disable()
+{
+}
---
+++ 3D_Engine/ComponentMesh.h
@@ -0,0 +1,27 @@
+#ifndef __COMPONENTMESH_H__
+#define __COMPONENTMESH_H__
+
+#include "Component.h"
+
+class Gameobject;
+
+class ComponentMesh : public Component
+{
+public:
+	ComponentMesh(Gameobject* owner,CompType newtype);
+	virtual ~ComponentMesh();
+
+	void Init();
+	void Update();
+	void CleanUp();
+
+	void Enable();
+	void Disable();
+
+
+private:
+	
+
+};
+
+#endif
